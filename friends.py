f5480039f15eae46c62c9673b560983ae2264475
dr-robin <robinmcgregor@gmail.com>
2022-01-14 23:01:43 +0000

    limited to season 1

--- friends.py
+++ friends.py
@@ -27,7 +27,7 @@ def main(df):
 		
 	select_pos = st.sidebar.multiselect(
      		'Parts of Speech',
-     		['SCONJ', 'PRON', 'VERB', 'PUNCT', 'AUX', 'PART', 'NOUN','DET', 'ADJ', 'PROPN', 'ADP', 'ADV', 			'CCONJ','INTJ', 'NUM','SYM', 'X'],
+     		['SCONJ', 'PRON', 'VERB', 'PUNCT', 'AUX', 'PART', 'NOUN','DET', 'ADJ', 'PROPN', 'ADP', 'ADV','CCONJ','INTJ', 'NUM','SYM', 'X'],
      		['VERB', 'ADJ','NOUN'])
 	select_level = st.sidebar.multiselect(
      	'English Level',
@@ -55,15 +55,6 @@ def main(df):
 	#plotpie(df)
 	plotbar(df)
 	
-	try:
-		plotbar(df)
-	except Exception as e:
-		st.write(e)
-		
-	try:
-		plotpie(df)
-	except:
-		pass
 		
 	for p in select_pos:
 		st.subheader(posdic[str(p)])
@@ -96,8 +87,9 @@ def createdic():
 	return dic
 
 def filesdic(files):
-	keys = [f.split('/')[-1].split('.')[0] for f in files]
+	keys = [f.split('/')[-1].split('(')[0] for f in files]
 	d = {k:v for k,v in zip(keys,files)}
+	d = {k:v for k,v in sorted(d.items())}
 	return d
 	
 def select_episode():
@@ -107,7 +99,7 @@ def select_episode():
 	options = list(d.keys())
 	selected_episode = st.sidebar.selectbox("Select episode", options=options, index=0, key=None, on_change=None)
 	#get filename for selected episode from dic
-	selected_episode = d[selected_episode]
+	selected_episode = d[str(selected_episode)]
 	return selected_episode
 
 def select_pos():
@@ -123,15 +115,12 @@ def getencoding(selected_episode):
 	encode = encode['encoding']
 	return encode
 def loadsubs(selected_episode):
-	#file = open(selected_episode, "rb")
-	#enc = chardet.detect(file.read())
-	#enc = enc['encoding']
-	#st.write(enc)
-	#file.close()
+
+	return 
+
+@st.cache
+def analyzesubs(selected_episode):
 	subs = pysrt.open(selected_episode)
-	return subs
-	
-def analyzesubs(subs):
 	allsubs = [s.text for s in subs]
 	corpus = ''.join(allsubs)
 
@@ -155,7 +144,7 @@ def analyzesubs(subs):
 	pos = [(token.text, token.pos_) for token in doc]
 	df = pd.DataFrame(pos, columns = ['word','pos'])
 
-	return df
+	return df, subs
 
 def analyze():
 	return None
@@ -201,10 +190,107 @@ posdic = {'VERB': 'verb','ADJ': 'adjective','ADV': 'adverb','AUX': 'auxiliary','
 'PRON': 'pronoun','PROPN': 'proper noun','PUNCT': 'punctuation','SCONJ': 'subordinating conjunction',
 'SYM': 'symbol','X': 'other', 'ADP': 'adposition'}
 
+codes = ["ascii",
+"big5",
+"big5hkscs",
+"cp037",
+"cp273",
+"cp424",
+"cp437",
+"cp500",
+"cp720",
+"cp737",
+"cp775",
+"cp850",
+"cp852",
+"cp855",
+"cp856",
+"cp857",
+"cp858",
+"cp860",
+"cp861",
+"cp862",
+"cp863",
+"cp864",
+"cp865",
+"cp866",
+"cp869",
+"cp874",
+"cp875",
+"cp932",
+"cp949",
+"cp950",
+"cp1006",
+"cp1026",
+"cp1125",
+"cp1140",
+"cp1250",
+"cp1251",
+"cp1252",
+"cp1253",
+"cp1254",
+"cp1255",
+"cp1256",
+"cp1257",
+"cp1258",
+"euc_jp",
+"euc_jis_2004",
+"euc_jisx0213",
+"euc_kr",
+"gb2312",
+"gbk",
+"gb18030",
+"hz",
+"iso2022_jp",
+"iso2022_jp_1",
+"iso2022_jp_2",
+"iso2022_jp_2004",
+"iso2022_jp_3",
+"iso2022_jp_ext",
+"iso2022_kr",
+"latin_1",
+"iso8859_2",
+"iso8859_3",
+"iso8859_4",
+"iso8859_5",
+"iso8859_6",
+"iso8859_7",
+"iso8859_8",
+"iso8859_9",
+"iso8859_10",
+"o8859_11",
+"iso8859_13",
+"iso8859_14",
+"iso8859_15",
+"iso8859_16",
+"johab",
+"koi8_r",
+"koi8_t",
+"koi8_u",
+"kz1048",
+"mac_cyrillic",
+"mac_greek",
+"mac_iceland",
+"mac_latin2",
+"mac_roman",
+"mac_turkish",
+"ptcp154",
+"shift_jis",
+"shift_jis_2004",
+"shift_jisx0213",
+"utf_32",
+"utf_32_be",
+"utf_32_le",
+"utf_16",
+"utf_16_be",
+"utf_16_le",
+"utf_7",
+"utf_8",
+"utf_8_sig"]
+
 if __name__ == "__main__":
 	selected_episode = select_episode()
+	df, subs = analyzesubs(selected_episode)
 	
-	subs = loadsubs(selected_episode)
-	df = analyzesubs(subs)
 	main(df)
 
